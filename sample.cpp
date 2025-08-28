875e591042c4b83b4f9c1e1d623892d4712acd4b
tristan-G123 <tristangannon06@gmail.com>
2025-08-28 19:08:34 -0400

    Update sample.cpp

--- sample.cpp
+++ sample.cpp
@@ -3,5 +3,14 @@
 using namespace std;
 
 int main () {
-
+ //variables for first and second integers
+  int first, second;
+  //Displays text then returns user input
+  cout << "Enter first integer: ";
+  cin >> first;
+  cout << "Enter second integer: ";
+  cin >> second;
+  //adds both first and second together
+  int sum = first + second;
+  cout << "\nThe sum is " << sum << "." << endl;
 }
